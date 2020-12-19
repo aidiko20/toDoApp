fa22517e9522dfeb49c68a049bd055d9b4583092
Aida Yrysbekova <aidiko20@gmail.com>
2020-12-19 16:17:09 -0500

    added some logic

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
+const todos = document.getElementById("todos");
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const todo = input.nodeValue;
+    const todoText = input.nodeValue;
 
-    if(todo) {
-        
+    if(todoText) {
+        const todoEl = document.createElement("li");
+        todoEl.innerText = todoText;
+        todos.appendChild(todoEl);
+
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle("completed");
+        })
+
+        input.value = "";
     }
 
 })
